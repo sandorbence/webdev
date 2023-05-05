d5fda4cdccfd5441fd5852352a559a18d841196b
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-05-05 15:36:28 +0200

    finished drum projet

--- Practice/Drum Kit Starting Files/index.js
+++ Practice/Drum Kit Starting Files/index.js
@@ -1,26 +1,35 @@
 var sounds = {
-    w: "sounds/tom-1.mp3",
-    a: "sounds/tom-2.mp3",
-    s: "sounds/tom-3.mp3",
-    d: "sounds/tom-4.mp3",
-    j: "sounds/crash.mp3",
-    k: "sounds/kick-bass.mp3",
-    l: "sounds/snare.mp3"
-}
+  w: "sounds/tom-1.mp3",
+  a: "sounds/tom-2.mp3",
+  s: "sounds/tom-3.mp3",
+  d: "sounds/tom-4.mp3",
+  j: "sounds/crash.mp3",
+  k: "sounds/kick-bass.mp3",
+  l: "sounds/snare.mp3",
+};
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        playSound(this.textContent);
-    });
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    playSound(this.textContent);
+    buttonAnimation(this.textContent);
+  });
 }
 
 document.addEventListener("keydown", function (event) {
-    playSound(event.key);
+  playSound(event.key);
+  buttonAnimation(event.key);
 });
 
 function playSound(key) {
-    if (sounds.hasOwnProperty(key)) {
-        new Audio(sounds[key]).play();
-    }
-    else alert("Tedd anyádba! Faszt nyomogatsz?");
+  if (sounds.hasOwnProperty(key)) {
+    new Audio(sounds[key]).play();
+  } else alert("Tedd anyádba! Faszt nyomogatsz?");
+}
+
+function buttonAnimation(key) {
+  var pressedButton = document.querySelector("." + key);
+  pressedButton.classList.add("pressed");
+  setTimeout(function () {
+    pressedButton.classList.remove("pressed");
+  }, 100);
 }
