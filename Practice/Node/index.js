812f4d935a30b1180418a5fd282fa5c77f9a1a0f
sandorbence <szaniszlo47@gmail.com>
2023-05-08 21:58:48 +0200

    added express

--- Practice/Node/index.js
+++
@@ -1,8 +0,0 @@
-//jshint esversion:6
-
-var superheroes = require("superheroes");
-var supervillains = require("supervillains");
-
-var mySupe = superheroes.random();
-var myVillain = supervillains.random();
-console.log(mySupe + " vs " + myVillain);
