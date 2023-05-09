c1571ce74b7501c729c7fc5cfe78df6d34af302d
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-05-09 16:24:51 +0200

    express server practice

--- Practice/ExpressServer/server.js
+++ Practice/ExpressServer/server.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send("<h1>Nyeló</h1>");
+});
+
+app.get("/anyad", function (req, res) {
+  res.send("<h2>Tedd anyádba</h2>");
+});
+
+app.get("/about", function (req, res) {
+  res.send("<h1>Még mindig tedd anyádba</h1>");
+});
+
 app.listen(3000, function () {
-    console.log("server started on port 3000");
+  console.log("server started on port 3000");
 });
