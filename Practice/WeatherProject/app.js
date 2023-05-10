19f41a33147846e664bee5d0065e577a8b6550e1
Sandor Bence (AE/ESI-ML1-Bp) <Bence.Sandor2@hu.bosch.com>
2023-05-10 16:46:29 +0200

    progress in weather app

--- Practice/WeatherProject/app.js
+++ Practice/WeatherProject/app.js
@@ -1,23 +1,48 @@
 const express = require("express");
 const app = express();
 const https = require("https");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=Budapest&appid=370dafc785c3ea5d7f173e0f5dce3e53&units=metric";
-    https.get(url, function (response) {
-        console.log(response.statusCode);
+  res.sendFile(__dirname + "/index.html");
+});
 
-        response.on("data", function (data) {
-            const weatherData = JSON.parse(data);
-            console.log(weatherData);
-            const temp = weatherData.main.temp;
-            const description = weatherData.weather[0].description;
-            console.log(temp);
-        });
+app.post("/", function (req, res) {
+  const query = req.body.city;
+  const apiKey = "370dafc785c3ea5d7f173e0f5dce3e53";
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+  https.get(url, function (response) {
+    console.log(response.statusCode);
+
+    response.on("data", function (data) {
+      const weatherData = JSON.parse(data);
+      console.log(weatherData);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const icon = weatherData.weather[0].icon;
+      const iconUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+      res.write(
+        "The weather in Budapest is " +
+          description +
+          ", the temperature is " +
+          temp +
+          "."
+      );
+      res.write("<img src=" + iconUrl + "/>");
+      res.send();
     });
-    res.send("asd");
+  });
 });
 
 app.listen("3000", function () {
-    console.log("Server is running on port 3000.");
+  console.log("Server is running on port 3000.");
 });
